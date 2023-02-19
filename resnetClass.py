9dd736bd46753012e21b7d86bce48ac72a90ce3b
paterane <peteroomm@gmail.com>
2023-02-19 19:54:39 +0630

    foodNofoodClassifier

---
+++ resnetClass.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import load_model
+from PIL import Image
+import joblib
+import numpy as np
+
+class Classifier:
+    __model = load_model('resnet_model/')
+    __classes = joblib.load('output_classes.save')
+    def getImageData(image):
+        '''
+        Return: np.array of image data
+        '''
+        im = Image.open(image, 'r').resize((180,180))
+        im_data = np.array(im.getdata()).reshape((1,180,180,3))
+        return im_data
+    def showImage(image):
+        '''
+        It will show an image of the file specified
+        '''
+        im = Image.open(image, 'r').resize((180,180))
+        im.show()
+    def IsFood(image_data):
+        '''
+        Return: 'food' or 'non_food' out of classes
+        '''
+        predict = Classifier.__model.predict(image_data)
+        classes = Classifier.__classes[np.argmax(predict.reshape(-1,))]
+        return classes
---
+++ server.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from resnetClass import Classifier
+from PIL import Image
+import os
+
+images = os.listdir('images/')
+with st.sidebar:
+    st.markdown(':red[Drag and Drop] a sample **picture**')
+    for i in range(10):
+        im = Image.open(f'images/{images[i]}', 'r').resize((230,230))
+        st.image(im)
+
+st.markdown("# :blue[Select a picture, it will tell you whether food or not.]")
+image_file = st.file_uploader(":green[Upload Image file here]", 
+                              type=['jpg', 'png', 'jpeg'], 
+                              help='Drag and Drop an image',
+                              )
+col1, col2 = st.columns(2) 
+if image_file != None:
+    with col1:
+        st.image(image_file)
+    img_data = Classifier.getImageData(image_file)
+    prediction = Classifier.IsFood(img_data)
+    with col2:
+        if prediction == 'food':
+            st.markdown("## :green[The picture shows kind of food..]")
+        else:
+            st.markdown("## :green[It seems no food in the picture.]")
